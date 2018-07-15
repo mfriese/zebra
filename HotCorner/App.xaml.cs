214c359a32c8dfb73fe653a1f774372f94817a79
Markus <markus.friese@gmail.com>
2018-07-15 09:32:27 +0200

    Initial Commit

---
+++ HotCorner/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using GlobalLowLevelHooks;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotCorner
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected static Boolean isLocked = false;
+
+
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MouseHook mouseHook = new MouseHook();
+
+            // Capture mouse events
+            mouseHook.MouseMove += new MouseHook.MouseHookCallback(mouseHook_MouseMove);
+
+            mouseHook.Install();
+        }
+
+        private void mouseHook_MouseMove(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            // Unlock and exit if mouse is outside the hot area
+            if (mouseStruct.pt.x > 4 || mouseStruct.pt.y > 4)
+            {
+                isLocked = false;
+                return;
+            }
+
+            // Do not allow restarts. If hot corner is locked stop.
+            if (isLocked)
+                return;
+
+            // Hot corner was not locked, lock it now.
+            isLocked = true;
+
+            INPUT[] sequence = {
+                User32Util.MakeInput(KeyCode.LWIN, false),
+                User32Util.MakeInput(KeyCode.TAB, false),
+                User32Util.MakeInput(KeyCode.TAB, true),
+                User32Util.MakeInput(KeyCode.LWIN, true)
+            };
+
+            // Send the keystroke to windows, activate Mission Control.
+            User32Util.SendInputSequence(sequence);
+        }
+    }
+}
---
+++ HotCorner/User32Util.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HotCorner
+{
+    /// <summary>
+    /// Contains interface to native system calls.
+    /// </summary>
+    public static class User32Util
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint numberOfInputs, INPUT[] inputs, int sizeOfInputStructure);
+
+        /// <summary>
+        /// Send a single key to the system. The key will be held down until sendkeyup method is
+        /// called.
+        /// </summary>
+        /// <param name="keyCode">Representing the key to be held down.</param>
+        /// <param name="release">true, if the key is released, else false.</param>
+        internal static void SendInput(KeyCode keyCode, Boolean release)
+        {
+            INPUT[] inputs = new INPUT[] { MakeInput(keyCode, release) };
+            User32Util.SendInput(1, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        /// <summary>
+        /// Send a whole sequence of keys to the system.
+        /// </summary>
+        /// <param name="inputs">a sequence of key inputs.</param>
+        internal static void SendInputSequence(params INPUT[] inputs)
+        {
+            User32Util.SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        /// <summary>
+        /// Create the input structure. Necessary to create either a single or a sequence of
+        /// keystrokes.
+        /// </summary>
+        /// <param name="keyCode">Representing the key to be held down.</param>
+        /// <param name="release">true, if the key is released, else false.</param>
+        /// <returns>Native input structure wrapper.</returns>
+        internal static INPUT MakeInput(KeyCode keyCode, Boolean release)
+        {
+            INPUT input = new INPUT
+            {
+                Type = 1
+            };
+
+            input.Data.Keyboard = new KEYBDINPUT
+            {
+                Vk = (ushort)keyCode,
+                Scan = 0,
+                Flags = (uint)(release ? 2 : 0),
+                Time = 0,
+                ExtraInfo = IntPtr.Zero
+            };
+
+            return input;
+        }
+    }
+}
