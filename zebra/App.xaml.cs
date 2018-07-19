600d3f9d618e87883141d5caa0b16e3a9d66a311
Markus <markus.friese@explicatis.com>
2018-07-19 08:47:45 +0200

    Getting more ideas on what is needed to accelerate windows usage. Added Esc-Esc-Esc to trigger Alt+F4 cause it is the obvious Emacs way. Planning to add a UI to configure shortcuts and hot corners. And most important clean the code up.

--- zebra/App.xaml.cs
+++ zebra/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using GlobalLowLevelHooks;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows;
 
@@ -12,32 +13,68 @@ namespace HotCorner
     {
         protected static Boolean isLocked = false;
 
+        protected static List<KeyboardHook.VKeys> releasedKeys = new List<KeyboardHook.VKeys>();
+
+        protected static MouseHook mouseHook = null;
+
+        protected static KeyboardHook keyHook = null;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            MouseHook mouseHook = new MouseHook();
+            mouseHook = new MouseHook();
+            keyHook = new KeyboardHook();
 
             // Capture mouse events
             mouseHook.MouseMove += new MouseHook.MouseHookCallback(mouseHook_MouseMove);
-           
+            keyHook.KeyUp += KeyHook_KeyUp;
+
             var bitmap = new Bitmap("./icon.png");
             var iconHandle = bitmap.GetHicon();
             var icon = System.Drawing.Icon.FromHandle(iconHandle);
 
             var notifyIcon = new System.Windows.Forms.NotifyIcon();
-            notifyIcon.Click += new EventHandler(notifyIcon_Click);
+            notifyIcon.Click += new EventHandler(NotifyIcon_Click);
             notifyIcon.Icon = icon;
             notifyIcon.Visible = true;
 
             mouseHook.Install();
+            keyHook.Install();
         }
 
-        void notifyIcon_Click(object sender, EventArgs e)
+        private void KeyHook_KeyUp(KeyboardHook.VKeys key)
         {
+            if (key != KeyboardHook.VKeys.ESCAPE)
+            {
+                releasedKeys.Clear();
+                return;
+            }
+
+            releasedKeys.Add(key);
+
+            if (releasedKeys.Count > 2)
+            {
+                releasedKeys.Clear();
+                INPUT[] sequence = {
+                    User32Util.MakeInput(KeyCode.ALT, false),
+                    User32Util.MakeInput(KeyCode.F4, false),
+                    User32Util.MakeInput(KeyCode.F4, true),
+                    User32Util.MakeInput(KeyCode.ALT, true)
+                };
+                User32Util.SendInputSequence(sequence);
+            }
+        }
+
+        void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            mouseHook.Uninstall();
+            keyHook.Uninstall();
             this.Shutdown();
         }
 
         private void mouseHook_MouseMove(MouseHook.MSLLHOOKSTRUCT mouseStruct)
         {
+            Console.WriteLine(String.Format("{0} : {1}", mouseStruct.pt.x, mouseStruct.pt.y));
+
             // Unlock and exit if mouse is outside the hot area
             if (mouseStruct.pt.x > 4 || mouseStruct.pt.y > 4)
             {
